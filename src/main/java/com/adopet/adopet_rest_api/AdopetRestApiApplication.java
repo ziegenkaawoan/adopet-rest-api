3966e8399476ebc3914830d648fc2bc2ec2d39c4
Jonathan K <jkamagi41@gmail.com>
2025-03-29 20:18:34 +0800

    Initial Commit

---
+++ src/main/java/com/adopet/adopet_rest_api/AdopetRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.adopet.adopet_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopetRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopetRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adopet/adopet_rest_api/AdopetRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adopet.adopet_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopetRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
